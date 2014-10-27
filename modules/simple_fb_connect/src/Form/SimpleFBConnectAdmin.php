f69a449f4667d9cbd5bbd4cafe1dcb83c660d0be
Tanay Sai <saitanay@gmail.com>
2014-10-27 17:30:33 +0530

    added form deafult values and formsubmit handler

--- modules/simple_fb_connect/src/Form/SimpleFBConnectAdmin.php
+++ modules/simple_fb_connect/src/Form/SimpleFBConnectAdmin.php
@@ -3,7 +3,7 @@
 namespace Drupal\simple_fb_connect\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
 
 
 
@@ -23,10 +23,53 @@ class SimpleFBConnectAdmin extends ConfigFormBase{
             '#type' => 'textfield',
             '#required' => TRUE,
             '#title' => t('Application ID'),
+            '#default_value' => \Drupal::config('simple_fb_connect.settings')->get('simple_fb_connect_appid'),
             '#description' => t('Also called the <em>OAuth client_id</em> value on Facebook App settings pages. <a href="https://www.facebook.com/developers/createapp.php">Facebook Apps must first be created</a> before they can be added here.'),
         );
 
+        $form['simple_fb_connect_skey'] = array(
+            '#type' => 'textfield',
+            '#required' => TRUE,
+            '#title' => t('Application Secret'),
+            '#default_value' => \Drupal::config('simple_fb_connect.settings')->get('simple_fb_connect_skey'),
+            '#description' => t('Also called the <em>OAuth client_secret</em> value on Facebook App settings pages.'),
+        );
+
+
+        $form['simple_fb_connect_connect_url'] = array(
+            '#type' => 'textfield',
+            '#attributes' => array('readonly' => 'readonly'),
+            '#title' => t('Connect url'),
+            '#description' => t('Copy this value into Facebook Applications on Connect settings tab'),
+            '#default_value' => $GLOBALS['base_url'],
+        );
+
+        $form['simple_fb_connect_login_only'] = array(
+            '#type' => 'checkbox',
+            '#title' => t('Login Only (No Registration)'),
+            '#description' => t('Allow only existing users to login with FB. New users can not signup using FB Connect.'),
+            '#default_value' => \Drupal::config('simple_fb_connect.settings')->get('simple_fb_connect_login_only'),
+        );
+
+        $form['simple_fb_connect_post_login_url'] = array(
+            '#type' => 'textfield',
+            '#title' => t('Post Login url'),
+            '#description' => t('Drupal URL to which the user should be redirected to after successful login.'),
+            '#default_value' => \Drupal::config('simple_fb_connect.settings')->get('simple_fb_connect_post_login_url'),
+        );
+
         return parent::buildForm($form, $form_state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        dpm($form_state->getValue('simple_fb_connect_appid'));
+        dpm($form_state->getValue('simple_fb_connect_skey'));
+        \Drupal::config('simple_fb_connect.settings')->set('simple_fb_connect_appid',$form_state->getValue('simple_fb_connect_appid'));
+        \Drupal::config('simple_fb_connect.settings')->set('simple_fb_connect_skey',$form_state->getValue('simple_fb_connect_skey'));
+        drupal_set_message($this->t('The configuration options have been saved.'));
     }
 }
 
